63a6846b15b508b5188d5c03e01834e1fa24fa13
Suraj Aryal <aryalsuraj723@gmail.com>
2025-04-20 22:18:39 +0545

    Complete page layout

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -20,6 +20,8 @@ import { FontSizeExtension } from "@/extensions/font-size";
 import { LineHeightExtension } from "@/extensions/line-height";
 import { useEditorStore } from "@/app/store/use-editor-store";
 
+
+
 export const Editor = () => {
   const { setEditor } = useEditorStore();
 
@@ -109,6 +111,9 @@ export const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-[#f9fbfd] px-4 print:p-0 print:bg-white print:overflow-visible">
+      
+      
+      
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
