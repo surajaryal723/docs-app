df0c26334926e39a37cee721608ec92887d72e9f
Suraj Aryal <aryalsuraj723@gmail.com>
2025-04-22 19:07:54 +0545

    Bootstrap navbar

---
+++ src/app/documents/[documentId]/navbar.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import { DocumentInput } from "./document-input";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { FileIcon } from "lucide-react";
+
+export const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between">
+      <div className="flex gap-2 items-center">
+        <Link href={"/"}>
+          <Image src={"/logo.svg"} alt="Logo" height={36} width={36} />
+        </Link>
+        <div className="flex flex-col">
+          <DocumentInput />
+          <div className="flex">
+            <Menubar className="border-none bg-transparent shadow-none h-auto p-0">
+              <MenubarMenu>
+                <MenubarTrigger className="cursor-pointer">File</MenubarTrigger>
+                <MenubarContent>
+                  <MenubarItem>
+                    <FileIcon className="size-4 mr-2" /> Save
+                  </MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+            </Menubar>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,5 +1,6 @@
 import { Editor } from "./editor";
-import {Toolbar} from "./toolbar"
+import { Navbar } from "./navbar";
+import { Toolbar } from "./toolbar";
 interface DocumentIdProps {
   params: Promise<{ documentId: string }>;
 }
@@ -9,9 +10,15 @@ export default async function IndividualDocument({ params }: DocumentIdProps) {
   console.log(slug);
   return (
     <div className="min-h-screen bg-[#fafbfd]">
-      <Toolbar/>
+      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#fafbfd] print:hidden">
+        {" "}
+        <Navbar />
+        <Toolbar />
+      </div>
+      <div className="pt-[114px] print:pt-0 ">
+
       <Editor />
-      
+      </div>
     </div>
   );
 }
