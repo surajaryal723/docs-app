e55aa44203b3c61b5d375ce6549e61c049322063
Suraj Aryal <aryalsuraj723@gmail.com>
2025-04-27 20:59:15 +0545

    Implement search input feature

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import { Navbar } from "./navbar"
 
 export default function Home(){
-  return <div className="w-full min-h-screen flex items-center justify-center">
+  return <div className="min-h-screen flex flex-col">
+    <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
+      <Navbar/>
+    </div>
+    <div className="mt-16">
     <h1 className="text-4xl">Hello</h1>
     <Button variant={"destructive"} size={"lg"} className="">Click Me</Button>
     <Link href={"/documents/123"}>Document</Link>
+    </div>
   </div>
 }
---
+++ src/hooks/use-search-param.ts
@@ -0,0 +1,5 @@
+import {parseAsString,useQueryState} from "nuqs"
+
+export function useSearchParam(key:string){
+    return useQueryState(key,parseAsString.withDefault("").withOptions({clearOnDefault:true}))
+}
