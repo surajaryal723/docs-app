81bdc1d640d85e92812fe3ded5b507fe3be5bb5c
Suraj Aryal <aryalsuraj723@gmail.com>
2025-05-01 18:40:14 +0545

    Initialize convex and define documents schema

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,30 @@
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
-import { Navbar } from "./navbar"
-import { TemplateGallery } from "./template-gallery"
+"use client";
+import { useQuery } from "convex/react";
+import { Navbar } from "./navbar";
+import { TemplateGallery } from "./template-gallery";
+import { api } from "../../../convex/_generated/api";
 
+export default function Home() {
+  const documents = useQuery(api.documents.get);
 
-export default function Home(){
-  return <div className="min-h-screen flex flex-col">
-    <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
-      <Navbar/>
+  if(documents===undefined){
+    return (
+      <p>Loading...</p>
+    )
+  }
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
+        <Navbar />
+      </div>
+      <div className="mt-16">
+        <TemplateGallery />
+        {documents?.map((document) => {
+         return  (
+          <span key={document._id}>{document.title}</span>
+         )
+        })}
+      </div>
     </div>
-    <div className="mt-16">
-    <TemplateGallery/>
-    </div>
-  </div>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import {NuqsAdapter} from "nuqs/adapters/next/app"
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
 
-const inter =  Inter({
-subsets:["latin"]
-})
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,11 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={inter.className}
-      >
+      <body className={inter.className}>
         <NuqsAdapter>
-        {children}
+          <ConvexClientProvider>{children}</ConvexClientProvider>
         </NuqsAdapter>
       </body>
     </html>
