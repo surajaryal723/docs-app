d671cc2996e2974b41a714133b6ad72982b19fc4
Suraj Aryal <aryalsuraj723@gmail.com>
2025-05-11 20:20:56 +0545

    Add search functionality

--- convex/documents.ts
+++ convex/documents.ts
@@ -5,73 +5,88 @@ import { paginationOptsValidator } from "convex/server";
 export const create = mutation({
   args: {
     title: v.optional(v.string()),
-    initialContent:v.optional(v.string())
+    initialContent: v.optional(v.string()),
   },
-  handler: async (ctx,args) => {
-    const user = await ctx.auth.getUserIdentity()
-    if(!user){
-      throw new ConvexError("Unauthorized")
+  handler: async (ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
+    if (!user) {
+      throw new ConvexError("Unauthorized");
     }
-    const documentId = await ctx.db.insert("documents",{
-      title:args.title ?? "Untitled document",
-      ownerId : user.subject,
-      initialContent : args.initialContent
-    })
+    const documentId = await ctx.db.insert("documents", {
+      title: args.title ?? "Untitled document",
+      ownerId: user.subject,
+      initialContent: args.initialContent,
+    });
 
-    return documentId
-
+    return documentId;
   },
 });
 
 export const get = query({
-  args: {paginationOpts:paginationOptsValidator},
-  handler: async (ctx,args) => {
-    return await ctx.db.query("documents").paginate(args.paginationOpts);
+  args: {
+    paginationOpts: paginationOptsValidator,
+    search: v.optional(v.string()),
+  },
+  handler: async (ctx, { search, paginationOpts }) => {
+    const user = await ctx.auth.getUserIdentity();
+    if (!user) {
+      throw new ConvexError("Unauthorized");
+    }
+    if (search) {
+      return await ctx.db
+        .query("documents")
+        .withSearchIndex("search_title", (q) =>
+          q.search("title", search).eq("ownerId", user.subject)
+        )
+        .paginate(paginationOpts)
+    }
+
+    return await ctx.db.query("documents").withIndex("by_owner_id",(q)=>q.eq("ownerId",user.subject)).paginate(paginationOpts);
   },
 });
 
 export const removeById = mutation({
-  args:{id:v.id("documents")},
-  handler:async (ctx,args)=>{
-    const user = await ctx.auth.getUserIdentity()
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
 
-      if(!user){
-        throw new ConvexError("Unauthorized")
-      }
-    const document  = await ctx.db.get(args.id)
+    if (!user) {
+      throw new ConvexError("Unauthorized");
+    }
+    const document = await ctx.db.get(args.id);
 
-    if(!document){
-      throw new ConvexError("Document not found")
+    if (!document) {
+      throw new ConvexError("Document not found");
     }
 
-    const isOwner = document.ownerId ===user.subject
+    const isOwner = document.ownerId === user.subject;
 
-    if(!isOwner){
-      throw new ConvexError("Unauthorized")
+    if (!isOwner) {
+      throw new ConvexError("Unauthorized");
     }
-    return await ctx.db.delete(args.id)
-  }
-})
+    return await ctx.db.delete(args.id);
+  },
+});
 
 export const updateById = mutation({
-  args:{id:v.id("documents"),title:v.string()},
-  handler:async (ctx,args)=>{
-    const user = await ctx.auth.getUserIdentity()
+  args: { id: v.id("documents"), title: v.string() },
+  handler: async (ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
 
-      if(!user){
-        throw new ConvexError("Unauthorized")
-      }
-    const document  = await ctx.db.get(args.id)
+    if (!user) {
+      throw new ConvexError("Unauthorized");
+    }
+    const document = await ctx.db.get(args.id);
 
-    if(!document){
-      throw new ConvexError("Document not found")
+    if (!document) {
+      throw new ConvexError("Document not found");
     }
 
-    const isOwner = document.ownerId ===user.subject
+    const isOwner = document.ownerId === user.subject;
 
-    if(!isOwner){
-      throw new ConvexError("Unauthorized")
+    if (!isOwner) {
+      throw new ConvexError("Unauthorized");
     }
-    return await ctx.db.patch(args.id,{title:args.title})
-  }
-})
+    return await ctx.db.patch(args.id, { title: args.title });
+  },
+});
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,12 +4,16 @@ import { Navbar } from "./navbar";
 import { TemplateGallery } from "./template-gallery";
 import { api } from "../../../convex/_generated/api";
 import { DocumentsTable } from "./documents-table";
-
+import { useSearchParam } from "@/hooks/use-search-param";
 
 export default function Home() {
-  const {results,status,loadMore} = usePaginatedQuery(api.documents.get,{},{initialNumItems:5});
+  const [search, setSearch] = useSearchParam("search");
+  const { results, status, loadMore } = usePaginatedQuery(
+    api.documents.get,
+    {search},
+    { initialNumItems: 5 }
+  );
 
-  
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -18,9 +22,9 @@ export default function Home() {
       <div className="mt-16">
         <TemplateGallery />
         <DocumentsTable
-        documents={results}
-        loadMore={loadMore}
-        status={status}
+          documents={results}
+          loadMore={loadMore}
+          status={status}
         />
       </div>
     </div>
